f1fb31ef466c758ea397c5ae73df5f270b50022c
arman324 <arman324@users.noreply.github.com>
2021-07-10 00:08:55 +0430

    Update init.c

--- init.c
+++ init.c
@@ -2,6 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+void initData();
+
 void initialize(){
   printf("Init\n");
+
+  initData();
+
+}
+
+void initData(){
+  int popSize;
+  while(1){
+    printf("Enter population size (use 50 or 100) - popSize-> ");
+    scanf("%d", &popSize);
+    if (popSize == 50 || popSize == 100){
+      printf("population size is : %d\n",popSize);
+      break;
+    }
+    else{
+      printf("\nNote population size must be 50 or 100\n");
+      continue;
+    }
+  }
+  printf("here\n");
+
 }
