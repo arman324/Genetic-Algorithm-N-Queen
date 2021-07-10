ca4b98c537bec7cd7e812a767e4f856332898896
arman324 <arman324@users.noreply.github.com>
2021-07-11 00:26:30 +0430

    Update gen.c

--- gen.c
+++ gen.c
@@ -6,6 +6,8 @@ int roulette(int sumFitness, int popSize);
 void generation(){
 
   int p1, p2;
+  int om1[50] = {0};
+  int om2[50] = {0};
 
   for (int i=0; i < popSize; i++){
     normal_fitness = (fitness[i]/sumFitness);
@@ -13,12 +15,28 @@ void generation(){
     comulative_fitness[i] = comulative;
   }
 
-  p1 = roulette(sumFitness, popSize);
-  p2 = roulette(sumFitness, popSize);
+  for(int i = 0; i < popSize; i += 2){
 
-  for (int i = 0; i< lchrom; i++){
-    printf("%d ", population[p1][i]);
+    p1 = roulette(sumFitness, popSize);
+    p2 = roulette(sumFitness, popSize);
+
+    for (int i = 0; i< lchrom; i++){
+      om1[i] = population[p1][i];
+      om2[i] = population[p2][i];
+    }
+
+    for (int i = 0; i< lchrom; i++){
+      printf("%d ", om1[i]);
+    }
+
+    printf("\n");
+
+    for (int i = 0; i< lchrom; i++){
+      printf("%d ", om2[i]);
+    }
+
+    printf("\n");
+    //crossover
   }
-  printf("\n");
 
 }
